f42243d016386b2c99eebdcfd8ca6c45ddc4d08d
Fred Pauchet <fpauchet@gmail.com>
2012-03-31 00:19:06 +0200

    model is ok, import misses the pictures

--- cards/models.py
+++ cards/models.py
@@ -3,22 +3,31 @@
 from django.db import models
 
 class Country(models.Model):
-	name = models.CharField(verbose_name='Pays', max_length=50)
+	name = models.CharField(verbose_name='Pays', unique=True, max_length=50)
 	
 	class Meta:
 		verbose_name_plural ='Pays'
 		
+	def __unicode__(self):
+		return self.name
+		
 class Category(models.Model):
-	label = models.CharField(verbose_name='Catégorie', max_length=50)
+	label = models.CharField(verbose_name='Catégorie', unique=True,  max_length=50)
 	
 	class Meta:
 		verbose_name_plural ='Catégories'
+		
+	def __unicode__(self):
+		return self.label
 
 class SubCategory(models.Model):
-	label = models.CharField(verbose_name='Thème', max_length=50)
+	label = models.CharField(verbose_name='Thème', unique=True, max_length=50)
 	
 	class Meta:
 		verbose_name_plural ='Thèmes'
+		
+	def __unicode__(self):
+		return self.label
 
 class Item(models.Model):
 	
@@ -27,12 +36,15 @@ class Item(models.Model):
 	subcategory = models.ForeignKey(SubCategory, verbose_name='Thème')
 	label = models.CharField(verbose_name='Titre', max_length=50)
 	image = models.ImageField(upload_to='pictures')
-	with_chip = models.BooleanField(verbose_name='Avec ou sans puce')
-	units = models.IntegerField(verbose_name="Nombre d'unités")
-	emissionDate = models.DateTimeField(verbose_name="Année d'émission")
-	expirationDate = models.DateTimeField(verbose_name="Année d'expiration")
-	numberOfCopies = models.IntegerField(verbose_name='Tirage')
+	with_chip = models.BooleanField(verbose_name='Avec ou sans puce', blank=True)
+	units = models.CharField(verbose_name="Nombre d'unités", max_length=50, blank=True)
+	emissionDate = models.CharField(verbose_name="Année d'émission", max_length=10, blank=True)
+	expirationDate = models.CharField(verbose_name="Année d'expiration", max_length=10, blank=True)
+	numberOfCopies = models.CharField(verbose_name='Tirage', max_length=50, blank=True)
 	
 	
 	class Meta:
 		verbose_name_plural ='Items'
+	
+	def __unicode__(self):
+		return '%s %s %s %s' % (self.country.name, self.category.label, self.subcategory.label, self.label)
--- data/importProcess.py
+++ data/importProcess.py
@@ -11,11 +11,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # a Django project. If this module is imported outside of a project
 # some environment needs to be set up. See
 # http://jystewart.net/process/2008/02/using-the-django-orm-as-a-standalone-component/.
-if 'DJANGO_SETTINGS_MODULE' not in os.environ:
-	sys.path.insert(0, (os.path.dirname(__file__) or '.') + '/../..') # put settings.py on path
-	os.environ['DJANGO_SETTINGS_MODULE'] = 'db.settings'
+# if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+	# sys.path.insert(0, (os.path.dirname(__file__) or '.') + '/../..') # put settings.py on path
+	# os.environ['DJANGO_SETTINGS_MODULE'] = 'db.settings'
+
+# from django.db import models
+
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+
+from django.conf import settings
 
 from django.db import models
+from cards.models import *
+
 
 class GCItem():
 	"""
@@ -40,6 +48,7 @@ class GCItem():
 	expirationDate = None
 	numberOfCopies = None
 	category = None
+	gcsid = None
 	
 	def __str__(self):
 		return u'%s / %s / %s / %s' % (self.country, self.category, self.subcategory, self.image_path)
@@ -68,7 +77,7 @@ def process():
 			 >
 		</collection>
 	"""
-	element = etree.parse(r'C:\Dev\Extras\xcards\data\collection.gcs')
+	element = etree.parse(r'C:\Dev\xcards\data\collection.gcs')
 	items = element.xpath('//collection/item')
 	
 	my_list = list()
@@ -87,17 +96,42 @@ def process():
 		i.expirationDate = attrs.get('gcsfield8')
 		i.numberOfCopies = attrs.get('gcsfield10')
 		i.category = attrs.get('gcsfield11')
-		
+		i.gcsid = attrs.get('gcsautoid')
+
+
 		my_list.append(i)
 	
 	return my_list
 	
 def insert(aList):
 	for item in aList:
-		pass
+		
+		print item.gcsid + ' ' + item.country
 
-	pass
-	
+		# i = Item.objects.get(label=item.label)
+
+		# if i is not None:
+		# 	continue
+
+		i = Item() #Item.objects.get_or_create(label=item.label)
+
+		country, created = Country.objects.get_or_create(name=item.country)
+		i.country = country
+
+		category, created = Category.objects.get_or_create(label=item.category)
+		i.category = category
+
+		subcategory, created = SubCategory.objects.get_or_create(label=item.subcategory)
+		i.subcategory = subcategory
+
+		i.units = item.units
+		i.label = item.label
+		i.emissionDate = item.emissionDate
+		i.expirationDate = item.expirationDate
+		i.numberOfCopies = item.numberOfCopies
+
+		i.save()
+		
 if __name__ == "__main__":
 	l = process()
 	insert(l)
