9eefc9f25e0e71949a4fa2ae7ea8754c99d105d4
Fred Pauchet <fpauchet@gmail.com>
2012-09-03 23:26:33 +0200

    Add autocomplete function from http://code.google.com/p/django-autocomplete/into the admin pages (card item modifications)

--- cards/admin.py
+++ cards/admin.py
@@ -1,8 +1,14 @@
 from cards.models import *
 from django.contrib import admin
-
-
+from xcards.autocomplete.widgets import *
 
 admin.site.register(Country)
 admin.site.register(Tag)
-admin.site.register(Card)
+
+class CardAdmin(AutocompleteModelAdmin):
+	related_search_fields = {
+		'country' : ('label',),
+		'tags' : ('label',),
+	}
+
+admin.site.register(Card, CardAdmin)
