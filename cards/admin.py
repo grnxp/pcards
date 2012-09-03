c22af6d87be65698103853d78afcc54a46386cfd
Fred Pauchet <fpauchet@gmail.com>
2012-09-03 20:42:35 +0200

    Delete the subcategory fields (continues...)

--- cards/admin.py
+++ cards/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 
 admin.site.register(Country)
 admin.site.register(Tag)
-
-class CardAdmin(admin.ModelAdmin):
-	exclude = ['subcategory']
-
-admin.site.register(Card, CardAdmin)
+admin.site.register(Card)
